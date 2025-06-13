f880b5e8c2737d148961bdd8a55006b7c8d10ddb
Sonu Kumar <156998478+Sonukumar3552@users.noreply.github.com>
2025-06-13 16:10:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,238 @@
+// // var age=25;
+
+// // if(true){
+// //     console.log(age);
+// // }
+// function solve(){
+//     var age=25;
+//     console.log(age);
+// }
+// solve();
+// let a=10;
+// console.log(a);
+
+// const a=20;
+// console.log(a);
+
+// let a=10;
+// let b=5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log(5>30);
+// console.log(2==3);
+
+// let age=5;
+// let stetus=(age>18)?'i can vote':'i can not vote';
+// console.log(stetus);
+
+// age=53;
+// if (age>=18) {
+//     console.log("vote");
+// } else {
+//     console.log("cant vote");
+// }
+
+// let num=5;
+
+// if (num==1) {
+//     console.log("a");
+// }
+// else if(num==2){
+//     console.log("b");
+// }
+// else if(num==3){
+//     console.log("c");
+// }
+// else if(num==4){
+//     console.log("d");
+// }
+// else if(num==5){
+//     console.log("e");
+// }
+// else if(num==6){
+//     console.log("f");
+// }
+// else if(num==7){
+//     console.log("g");
+// }
+// else{
+//     console.log("h")
+// }
+
+// let num=3;
+
+// switch (num) {
+//     case 1:
+//         console.log("a")
+//         break;
+//     case 2:
+//         console.log("b")
+//         break;
+//     case 3:
+//         console.log("c")
+//         break;
+//     case 4:
+//         console.log("d")
+//         break;
+//     default:
+//         break;
+// }
+
+// for(i=1; i<=10; i++)
+// {
+//     console.log("sonu")
+// }
+
+// i=1;
+// while(i<=10)
+// {
+//     console.log("sonu");
+//     i++;
+// }
+
+// i=10;
+// do{
+    
+//     console.log("sonu");
+//       i++;
+// }
+// while(i<=10)
+  
+// let i=10;
+// do{
+//     console.log(i)
+//     i++;
+// }
+// while(i<50);
+
+// let firstName="sonu";
+// let lastName='maahi';
+// console.log(firstName);
+// let ans=firstName+lastName;
+// console.log(ans);
+// console.log(firstName.substring(1,4));
+// console.log(object);
+
+// function sayMyname(){
+//     console.log("sonu maahi")
+// }
+// sayMyname();
+
+// function printCounting()
+// {
+//     for(let i=1;i<100;i++){
+//         console.log(i);
+//     }
+// }
+// printCounting();
+
+// function printnum(num) {
+//     console.log(num);
+// }
+// printnum(50);
+
+// let obj = {
+//     name:"sonu",
+//     age: 22,
+//     weight: 65,
+//     height: "5ft 4in",
+//     greet: function(){
+//         console.log("hello")
+//     }
+// };
+// console.log(obj)
+// obj.greet();
+
+// console.log(typeof(obj))
+
+// let arr =[1,2,3,4,5];
+// let brr = new Array (1, true, 'sonu');
+// console.log(arr);
+// brr.push('maahi');
+// brr.pop('maahi')
+// brr.shift();
+// brr.unshift("maahi");
+// console.log(brr);
+
+// console.log(brr[1]);
+
+// let arr = [2,3,4,5,6,7]
+// let ansArray = arr.map((number)=>{
+//     return number*number;
+// })
+// console.log(ansArray);
+// let arr = [2,3,4,5,6,7];
+// let ans = arr.reduce((acc,curr)=>{
+//     return acc + curr;
+// },0);
+// console.log(ans);
+// let arr =[6 ,2 ,8 ,1 ,4 ,9 ,3 ,6 ,8 ,2 ,6];
+// arr.sort();
+// console.log(arr);
+// console.log(arr.indexOf(8));
+
+// let greet = function(){
+//     console.log("greeting for the day");
+// }
+// greet();
+
+
+
+// const arr =[
+//     function(a,b){
+//         return a+b;
+//     },
+//     function(a,b){
+//         return a-b;
+//     },
+//     function(a,b){
+//         return a*b;
+//     }
+// ]
+
+// let first = arr[2];
+// let ans = first(5,10);
+// console.log(ans);
+
+
+
+// let obj =  {
+//     age : 25,
+//     wt : 59,
+//     ht: 180,
+//     greet :()=>{
+//         console.log("hello duniya");
+//     }
+// }
+// console.log(obj.age);
+// obj.greet();
+
+
+
+
+// class human {
+//     age =13;
+//     wt=60;
+//     ht=190;
+//     walking(){
+//         console.log("i m walking");
+//     }
+//     running(){
+//         console.log("i m running");
+//     }
+// }
+
+// let obj = new human();
+// console.log(obj.age);
+
+// obj.walking();
+
+
+console.log(Math.PI);
+console.log(Math.max(34 ,54 ,2 ,67 ,86 ,34 ,46 ,8 ));
+console.log(Math.min(34 ,54 ,2 ,67 ,86 ,34 ,46 ,8 ));
