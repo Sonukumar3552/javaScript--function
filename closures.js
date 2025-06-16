b4e2881ac35d40e8b45da6d2be31758fe5893ea4
Sonu Kumar <156998478+Sonukumar3552@users.noreply.github.com>
2025-06-16 17:49:29 +0530

    Add files via upload

---
+++ closures.js
@@ -0,0 +1,19 @@
+// function outerfunction() {
+//   let name = "sonu maahi"; // name is a local variable created by init
+//   function innerFuncton() {
+//     let name = "pandit";
+//     // displayName() is the inner function, that forms a closure
+//     console.log(name); // use variable declared in the parent function
+//   }
+//   innerFuncton();
+// }
+// outerfunction();
+
+
+
+if (Math.random() > 0.5) {
+  var x = 1;
+} else {
+  var x = 2;
+}
+console.log(x);
